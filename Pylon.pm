fb9e29b02fb89f7e76465dde1ad765a11b41d6b1
Patrick Gillan <patrick@minorimpact.com>
2015-04-28 13:11:45 -0700

    Changed the add() function to support how the code is actually written.

--- Pylon.pm
+++ Pylon.pm
@@ -214,8 +214,10 @@ sub add {
     my $value = shift || 0;
     my $type = shift;
 
-    my $add_string = "add|$graph_id|$server|$value";
-    $add_string .= "|counter" if ($type);
+    # Hard to make sure that no value that isn't supported by the server accidentally gets through.
+    $type = "gauge" unless ($type eq "counter");
+    my $add_string = "add|$graph_id|$server|$value|$type";
+
     return $self->command($add_string);
 }
 
